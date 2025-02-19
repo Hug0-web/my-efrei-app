79518858b83ea387faba0b2400ea21f1567c0b0a
Hugo <hugo.lucas@efrei.net>
2025-02-19 12:05:10 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="loginPage">
+    <div></div>
+      <div class="App">
+          <form>
+            <h1>Connexion</h1>
+            <label class="form-row-1"> Email : 
+              <input type="email" name="email" />
+            </label>  
+            <label class="form-row-2"> Mot de passe :  
+              <input type="password" name="password" />
+            </label> 
+          </form>
+          
+      </div>
     </div>
   );
 }
