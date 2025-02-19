a548774c126cacd8cfbf1401c1e6bf3270bcffec
Hugo <hugo.lucas@efrei.net>
2025-02-19 16:49:12 +0100

    push

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,18 +1,27 @@
 import './login.css'
+import { useEffect, useState } from "react";
 
 export const Login = () => {
+    const [user, setUser] = useState({
+        username: "",
+        password: "",
+      });
+
+
     return(
         <div className="loginPage">
             <div class="empty"></div>
             <div class="App">
-                <form>
+                <form onSubmit={() => {
+
+                }}>
                     <h1>Connexion</h1>
                     <h2>Utiliser votre compte Efrei</h2>
                     <label class="form-row-1"> Email : 
-                    <input type="email" name="email" />
+                    <input type="email" name="email" onChange={()}/>
                     </label>  
                     <label class="form-row-2"> Mot de passe :  
-                    <input type="password" name="password" />
+                    <input type="password" name="password" onChange={()}/>
                     </label> 
                     <p>En me connectant, <b>j'accepte</b> les conditions d'utilisations du service SSO Efrei notamment en matière de données personnelles.</p>
                     <input type="submit" value="SE CONNECTER" class="button" />
